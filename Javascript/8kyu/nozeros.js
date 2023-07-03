05330b2cf981e6518085b4a019554ff64af6b12a
Rahmath-001 <rehamathullah7@gmail.com>
2023-07-03 12:40:33 +0530

    no-zeroes

--- Javascript/8kyu/nozeros.js
+++ Javascript/8kyu/nozeros.js
@@ -1,7 +1,17 @@
+import readlineSync from 'readline-sync'
+
 function noBoringZeros(n) {
-    // your code
-    n=n.split();
-    console.log(n)
+    let num = n.toString().split('');
+    let arr = []
+
+    for (let i=0;i<num.length;i++){
+        if (num[i] != 0){
+            arr[i] = num[i]
+        }
+    }
+
+    return parseInt(arr.join(""))
   }
-  noBoringZeros()
-  n= 1450
+ let n = readlineSync.questionInt("Input number : ");
+console.log(noBoringZeros(n));
+  
